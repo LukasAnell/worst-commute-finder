028af5e2a12a28bed06bdce51e8245733da728f7
LukasAnell <lukasmanell@gmail.com>
2026-07-26 01:03:39 +0900

    Implement function to get the top n worst segments from a list of TrafficSegment.

---
+++ src/worst_commute_finder/main.py
@@ -0,0 +1,19 @@
+from chicago_traffic.models import TrafficSegment
+
+
+def get_top_n_worst_segments(
+    traffic_segments: list[TrafficSegment], n: int
+) -> list[TrafficSegment]:
+    # filter traffic segments so only ones with data are present
+    valid_traffic_segments: list[TrafficSegment] = [
+        segment for segment in traffic_segments if segment.has_data
+    ]
+
+    # sort segments by their current speed
+    valid_traffic_segments.sort(key=lambda segment: segment.current_speed)
+
+    # only return the top n segments
+    if len(valid_traffic_segments) >= n:
+        return valid_traffic_segments[:n]
+
+    return valid_traffic_segments
