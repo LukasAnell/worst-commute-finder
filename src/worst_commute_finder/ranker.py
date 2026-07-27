c5848e79464a3432e9009ac61dc21f6c783c657b
LukasAnell <lukasmanell@gmail.com>
2026-07-27 14:43:58 +0900

    Implement start of get_top_n_worst_relative()

--- src/worst_commute_finder/ranker.py
+++ src/worst_commute_finder/ranker.py
@@ -1,6 +1,26 @@
 from chicago_traffic.models import TrafficSegment
 
 
+def get_top_n_worst_relative(
+    live_segments: list[TrafficSegment],
+    historical_segments: list[TrafficSegment],
+    n: int,
+) -> list[tuple[TrafficSegment, float]]:
+    if n <= 0:
+        raise ValueError("n must be a positive integer")
+
+    # filter valid segments
+    valid_live_segments: list[TrafficSegment] = [
+        segment for segment in live_segments if segment.has_data
+    ]
+
+    valid_historical_segments: list[TrafficSegment] = [
+        segment for segment in historical_segments if segment.has_data
+    ]
+
+    return list()
+
+
 def get_top_n_worst_segments(
     traffic_segments: list[TrafficSegment], n: int
 ) -> list[TrafficSegment]:
