540aea189bd5f85503edf3cc559d4c816482f6ef
LukasAnell <lukasmanell@gmail.com>
2026-07-27 15:16:19 +0900

    Create a dict containing each segment's historical speeds, checking that the weekday and hour updated roughly match

--- src/worst_commute_finder/ranker.py
+++ src/worst_commute_finder/ranker.py
@@ -18,6 +18,20 @@ def get_top_n_worst_relative(
         segment for segment in historical_segments if segment.has_data
     ]
 
+    # create a dict of historical segments mapping segment_id to a list of speed readings
+    historical_speed_dict: dict[int, list[float]] = {}
+
+    for segment in valid_historical_segments:
+        # check that the historical segment is from the same day of the week and roughly the same hour as the live segment
+        if (
+            segment.last_updated.weekday == segment.last_updated.weekday
+            and abs(segment.last_updated.hour - segment.last_updated.hour) <= 1
+        ):
+            if segment.segment_id not in historical_speed_dict:
+                historical_speed_dict[segment.segment_id] = []
+
+            historical_speed_dict[segment.segment_id].append(segment.current_speed)
+
     return list()
 
 
